3999da5412c99cd9b6da04b3c6f61a025792841f
Julia Banaszek <julia.m.banaszek@gmail.com>
2023-04-24 14:20:03 +0200

    Code improvement

--- frontend/src/components/Flights/FlightsNavbar.tsx
+++ frontend/src/components/Flights/FlightsNavbar.tsx
@@ -4,9 +4,12 @@ function FlightsNavbar() {
       <a href="#home" className="logo">
         <b>Fligo</b>
       </a>
-      <a title="disabled" className="disabled">About</a>
-      <a title="disabled" className="disabled">Contact</a>
-      <input type="text" placeholder="Search.." title="disabled" className="disabled"/>
+      <a title="disabled" className="disabled">
+        About
+      </a>
+      <a title="disabled" className="disabled">
+        Contact
+      </a>
     </div>
   );
 }
